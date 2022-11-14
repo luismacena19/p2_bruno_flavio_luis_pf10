cecf0ac24129cfaf042bd188472b19924438fe6b
Luis Macena <luis.macena@outlook.com>
2022-11-14 20:24:19 -0300

    Add telas folder

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Pesquisar from './telas/Pesquisar'
+import Historico from './telas/Historico';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React from 'react';
+import { Tab, Text, TabView } from '@rneui/themed';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => {
+const [index, setIndex] = React.useState(0);
+
+return (
+  <>
+    <Tab
+      value={index}
+      onChange={(e) => setIndex(e)}
+      indicatorStyle={{
+        backgroundColor: 'white',
+        height: 3,
+      }}
+      variant="primary"
+    >
+      <Tab.Item
+        title="Pesquisar"
+        titleStyle={{ fontSize: 12 }}
+        icon={{ name: 'timer', type: 'ionicon', color: 'white' }}
+      />
+      <Tab.Item
+        title="Historico"
+        titleStyle={{ fontSize: 12 }}
+        icon={{ name: 'heart', type: 'ionicon', color: 'white' }}
+      />
+
+    </Tab>
+
+    <TabView value={index} onChange={setIndex} animationType="spring">
+      <TabView.Item style={{ backgroundColor: 'red', width: '100%' }}>
+        <Text h1>Pesquisar</Text>
+      </TabView.Item>
+      <TabView.Item style={{ backgroundColor: 'blue', width: '100%' }}>
+        <Text>{Historico}</Text>
+      </TabView.Item>
+    </TabView>
+  </>
+);
+};
